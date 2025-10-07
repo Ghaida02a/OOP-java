a6be559a6d48d3a23883359f289b163f266f1237
Ghaida <ggmskaj@gmail.com>
2025-10-07 16:09:58 +0400

    Task: Generic OOP Based System solved

---
+++ OOP/src/Phone.java
@@ -0,0 +1,61 @@
+public class Phone {
+
+    private Integer id;
+    private String name;
+    private String brand;
+    private double price;
+    private String color;
+    private Integer quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
