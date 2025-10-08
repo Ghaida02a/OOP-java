7910f886e30994c04dc97e062a430830d9cd2028
Ghaida <ggmskaj@gmail.com>
2025-10-08 14:24:26 +0400

    Created 3 separate specialized classes

---
+++ OOP/src/FeaturePhone.java
@@ -0,0 +1,11 @@
+public class FeaturePhone {
+    private Integer batteryLife;
+
+    public Integer getBatteryLife() {
+        return batteryLife;
+    }
+
+    public void setBatteryLife(Integer batteryLife) {
+        this.batteryLife = batteryLife;
+    }
+}
---
+++ OOP/src/GamingPhone.java
@@ -0,0 +1,20 @@
+public class GamingPhone {
+    private Integer gpu;
+    private Integer screenRefreshRate;
+
+    public Integer getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(Integer gpu) {
+        this.gpu = gpu;
+    }
+
+    public Integer getScreenRefreshRate() {
+        return screenRefreshRate;
+    }
+
+    public void setScreenRefreshRate(Integer screenRefreshRate) {
+        this.screenRefreshRate = screenRefreshRate;
+    }
+}
